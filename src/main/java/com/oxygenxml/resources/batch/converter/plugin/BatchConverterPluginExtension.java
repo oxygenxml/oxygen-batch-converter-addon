962dbb3f112517c43bd9c464a44c7954f2de4f4b
cosmin_duna <duna.marius.cosmin@gmail.com>
2021-01-05 17:44:05 +0200

    EXM-45715: Added the actions in "Import" submenu.

--- src/main/java/com/oxygenxml/resources/batch/converter/plugin/BatchConverterPluginExtension.java
+++ src/main/java/com/oxygenxml/resources/batch/converter/plugin/BatchConverterPluginExtension.java
@@ -1,5 +1,6 @@
 package com.oxygenxml.resources.batch.converter.plugin;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.util.ArrayList;
@@ -22,9 +23,12 @@ import com.oxygenxml.resources.batch.converter.translator.Translator;
 import com.oxygenxml.resources.batch.converter.view.ConverterDialog;
 
 import ro.sync.exml.plugin.workspace.WorkspaceAccessPluginExtension;
+import ro.sync.exml.workspace.api.editor.page.ditamap.WSDITAMapEditorPage;
 import ro.sync.exml.workspace.api.standalone.MenuBarCustomizer;
 import ro.sync.exml.workspace.api.standalone.StandalonePluginWorkspace;
+import ro.sync.exml.workspace.api.standalone.actions.MenusAndToolbarsContributorCustomizer;
 import ro.sync.exml.workspace.api.standalone.ui.Menu;
+import ro.sync.ui.application.menu.IApplicationMenu;
 
 /**
  * Plugin extension - Resources Batch Converter
@@ -32,21 +36,25 @@ import ro.sync.exml.workspace.api.standalone.ui.Menu;
 public class BatchConverterPluginExtension implements WorkspaceAccessPluginExtension {
 
 	/**
-	 * The id of menu where converter action will be place.
+	 * The id of Tools menu where converter action will be place.
 	 */
-	private static final String MENU_ID = "Tools";
+	private static final String TOOLS_MENU_ID = "Tools";
+	
+	 /**
+   * The id of Import sub-menu where converter action will be place.
+   */
+  private static final String IMPORT_MENU_ID = "Import";
 
 	/**
 	 * The preceding menu item.
 	 */
-	private static final String PRECEDING_MENU_ITEM_ACTION_ID = "XML_to_JSON";
+	private static final String PRECEDING_TOOLS_MENU_ITEM_ACTION_ID = "XML_to_JSON";
 
 	/**
 	 * The new succeeding menu item.
 	 * PRECEDING_MENU_ITEM_ACTION_ID was moved in Oxygen 22.
 	 */
-	private static final String NEW_SUCCEEDING_MENU_ITEM_ACTION_ID = "Format_and_indent_files";
-
+	private static final String NEW_SUCCEEDING_TOOLS_MENU_ITEM_ACTION_ID = "Format_and_indent_files";
 	
 	/**
 	 * Menu that contains items with all converter actions.
@@ -72,7 +80,7 @@ public class BatchConverterPluginExtension implements WorkspaceAccessPluginExten
 	  actionsInserted = false;
 	  
 		// List with actions.
-		final Map<String, List<Action>> actions = createActionsMap(pluginWorkspaceAccess);
+		final Map<String, List<Action>> convertActions = createConvertActionsMap(pluginWorkspaceAccess);
 
 		// Create a menu with actions and add it to Oxygen
 		pluginWorkspaceAccess.addMenuBarCustomizer(new MenuBarCustomizer() {
@@ -83,14 +91,14 @@ public class BatchConverterPluginExtension implements WorkspaceAccessPluginExten
 			public void customizeMainMenu(JMenuBar mainMenuBar) {
 			  // Check if the actions are added. 
 			  if(!actionsInserted) {
-			    addActionsInMenuBar(mainMenuBar, actions, pluginWorkspaceAccess);
+			    addConvertActionsInMenuBar(mainMenuBar, convertActions, pluginWorkspaceAccess);
 			    actionsInserted = true;
 			  }
 			}
 		});
 
 		// add a menu with actions in contextual menu of ProjectManager
-		ProjectManagerEditor.addPopUpMenuCustomizer(pluginWorkspaceAccess, actions, translator);
+		ProjectManagerEditor.addPopUpMenuCustomizer(pluginWorkspaceAccess, convertActions, translator);
 
 	}
 
@@ -104,52 +112,58 @@ public class BatchConverterPluginExtension implements WorkspaceAccessPluginExten
 	}
 
 	/**
-	 * Add the given Actions in the given JMenuBar.
+	 * Add the given converter actions in the given JMenuBar.
 	 *
-	 * @param mainMenuBar
-	 *          The menuBar
-	 * @param actions
-	 *          List with actions to add.
-	 * @param pluginWorkspaceAccess
-	 *          The oxygen PluginWorkspaceAccess.
+	 * @param mainMenuBar           The menuBar
+	 * @param actions               Map with converter actions to add.
+	 * @param pluginWorkspaceAccess The oxygen PluginWorkspaceAccess.
 	 */
-	private void addActionsInMenuBar(JMenuBar mainMenuBar, Map<String, List<Action>> actions,
+	private void addConvertActionsInMenuBar(JMenuBar mainMenuBar, Map<String, List<Action>> actions,
 			StandalonePluginWorkspace pluginWorkspaceAccess) {
 
 		batchConvertMenu = new Menu(translator.getTranslation(Tags.MENU_TEXT, ""));
-
-		int indexToInsert = -1;
-		
-		// add actions in batch converterMenu.
+		Menu batchConverterMenuForImport = new Menu(translator.getTranslation(Tags.MENU_TEXT, ""));
 		BatchConverterPluginUtil.addActionsInMenu(batchConvertMenu, actions);
+		BatchConverterPluginUtil.addActionsInMenu(batchConverterMenuForImport, actions);
+		
+		int indexToInsertInTools = -1;
+		JMenu importSubmenu = null;
 
-		// get the number of items in MenuBar
 		int menuBarSize = mainMenuBar.getMenuCount();
-
-    // iterate over menus in menuBar
     for (int i = 0; i < menuBarSize; i++) {
-      // get the menu with index i
       JMenu currentMenu = mainMenuBar.getMenu(i);
       if(currentMenu != null) {
-        indexToInsert = searchForPosition(currentMenu, pluginWorkspaceAccess);
         
-        if (indexToInsert != -1) {
-          currentMenu.add(batchConvertMenu, indexToInsert);
+        if(importSubmenu == null) {
+          importSubmenu = searchForImportSubMenu(currentMenu, pluginWorkspaceAccess);
+          if (importSubmenu != null) {
+            importSubmenu.add(batchConverterMenuForImport, importSubmenu.getItemCount());
+          }
+        }
+        if (indexToInsertInTools == -1) {
+          indexToInsertInTools = searchForPositionInToolsMenu(currentMenu, pluginWorkspaceAccess);
+          if (indexToInsertInTools != -1) {
+            currentMenu.add(batchConvertMenu, indexToInsertInTools);
+          }
+        }
+        
+        if (indexToInsertInTools != -1 && importSubmenu != null) {
           break;
         }
       }
     }
 	}
-
-
+	
 /**
  * Search for position to insert the menu with actions in given menu.
- * @param menu The menu to search.
- * @param pluginWorkspaceAccess StandalonePluginWorkspace
+ * 
+ * @param menu                    The menu to search.
+ * @param pluginWorkspaceAccess   Access to plugin workspace.
+ * 
  * @return The index of menu, or 0 if the menu is good, but a position cannot be found
  *  or -1 if wasn't find the position.
  */
-  private int searchForPosition(JMenu menu, StandalonePluginWorkspace pluginWorkspaceAccess) {
+  private int searchForPositionInToolsMenu(JMenu menu, StandalonePluginWorkspace pluginWorkspaceAccess) {
     int index = -1;
     // iterate over menu items in currentMenu
     int sizeMenu = menu.getItemCount();
@@ -167,11 +181,11 @@ public class BatchConverterPluginExtension implements WorkspaceAccessPluginExten
             int indexOfSlash = actionID.indexOf('/');
 
             // check the menuNameId
-            if (MENU_ID.equals(actionID.substring(0, indexOfSlash))) {
+            if (TOOLS_MENU_ID.equals(actionID.substring(0, indexOfSlash))) {
             	index = 0;
               // the menuNameId is MENU_ID
               // check the menuItemActionID
-              if (PRECEDING_MENU_ITEM_ACTION_ID.equals(actionID.substring(indexOfSlash + 1))) {
+              if (PRECEDING_TOOLS_MENU_ITEM_ACTION_ID.equals(actionID.substring(indexOfSlash + 1))) {
                 // the MenuIdemActionId is PRECEDING_MENU_ITEM_ACTION_ID.
                 // this is the index
                 index = i + 1;
@@ -179,7 +193,7 @@ public class BatchConverterPluginExtension implements WorkspaceAccessPluginExten
               }
               // In Oxygen 22, the PRECEDING_MENU_ITEM_ACTION_ID was moved from this menu.
               // We need a new reference action id.
-              if (NEW_SUCCEEDING_MENU_ITEM_ACTION_ID.equals(actionID.substring(indexOfSlash + 1))) {
+              if (NEW_SUCCEEDING_TOOLS_MENU_ITEM_ACTION_ID.equals(actionID.substring(indexOfSlash + 1))) {
                 index = i - 1;
                 break;
               }
@@ -195,6 +209,39 @@ public class BatchConverterPluginExtension implements WorkspaceAccessPluginExten
     return index;
   }
 
+  /**
+   * Search for the "Import" submenu into the given menu.
+   * 
+   * @param menu                  The menu to search.
+   * @param pluginWorkspaceAccess StandalonePluginWorkspace
+   * 
+   * @return The "Import" submenu, or <code>null</code> if it cannot be found.
+   */
+  private JMenu searchForImportSubMenu(JMenu menu, StandalonePluginWorkspace pluginWorkspaceAccess) {
+    JMenu importSubmenu = null;
+    Component[] menuComponents = menu.getMenuComponents();
+    for (int i = 0; i < menuComponents.length; i++) {
+      Component currrentComponent = menuComponents[i];
+      if(currrentComponent instanceof JMenu) {
+        JMenu submenu = (JMenu) currrentComponent;
+        if(submenu.getItemCount() > 0) {
+          Action action = submenu.getItem(0).getAction();
+          if (action != null) {
+            String actionID = pluginWorkspaceAccess.getOxygenActionID(action);
+            // The actionId is in format: menuNameId/menuItemActionID
+            int indexOfSlash = actionID.indexOf('/');
+
+            // check the menuNameId
+            if (indexOfSlash != -1 && IMPORT_MENU_ID.equals(actionID.substring(0, indexOfSlash))) {
+              importSubmenu = submenu;
+              break;
+            }
+          }
+        }
+      }
+    }
+    return importSubmenu;
+  }
 	
 	/**
 	 * Create the Swing action which shows the converter according to converter
@@ -237,7 +284,7 @@ public class BatchConverterPluginExtension implements WorkspaceAccessPluginExten
 	 * @param pluginWorkspaceAccess
 	 * @return
 	 */
-	private Map<String, List<Action>> createActionsMap(StandalonePluginWorkspace pluginWorkspaceAccess) {
+	private Map<String, List<Action>> createConvertActionsMap(StandalonePluginWorkspace pluginWorkspaceAccess) {
 	  Map<String, List<Action>> toReturn = new HashMap<String, List<Action>>();
 		List<Action> dita = new ArrayList<Action>();
 		
